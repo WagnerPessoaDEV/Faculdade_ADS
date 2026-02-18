7ec1f0cca092cc69a4c669ff99d780fb8a48fbc8
Wagner Pessoa <wagnerpessoa.dev@gmail.com>
2026-02-18 11:39:06 -0300

    inicial

---
+++ Introducao_Programacao_De_Computadores/N3R/NC1/n3r_nc1_1.c
@@ -0,0 +1,14 @@
+#include  <stdio.h>
+
+int main() {
+    int idade = 18;
+    int resultado;
+
+
+
+idade >= 18 ? printf("Você é maior de idade.\n") : printf("Você é menor de idade.\n");
+
+    return 0;
+    
+
+}
---
+++ Introducao_Programacao_De_Computadores/N3R/NC1/n3r_nc1_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int temperatura = 30;
+    int resultado;
+
+temperatura >= 30 ? printf("Está CALOR!.\n") : printf("Está frio.\n");
+
+
+ return 0;
+
+}
