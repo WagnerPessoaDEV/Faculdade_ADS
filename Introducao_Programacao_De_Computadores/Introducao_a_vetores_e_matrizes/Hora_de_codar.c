529213af114646e1bd08dda72a6d5f4fee0b1015
Wagner Pessoa <wagnerpessoa.dev@gmail.com>
2026-02-18 17:17:13 -0300

    hora de codar

--- Introducao_Programacao_De_Computadores/Introducao_a_vetores_e_matrizes/Hora_de_codar.c
+++ Introducao_Programacao_De_Computadores/Introducao_a_vetores_e_matrizes/Hora_de_codar.c
@@ -1,6 +1,30 @@
-#include    <stdio.h>
+#include <stdio.h>
 
 int main() {
+    int index;
+    
+    // Matriz de strings (ponteiros para char)
+    char *nomesAlunos[3][3] = {
+        {"Manuella", "Português: 30", "Matemática: 90"},
+        {"Valemtin", "Português: 60", "Matemática: 60"},
+        {"Miguel", "Português: 90", "Matemática: 30"},
+    };
+
+    printf("Digite o numero do aluno (0 a 2): ");
+    if (scanf("%d", &index) != 1) {
+        printf("Erro: Digite apenas números.\n");
+        return 1;
+    }
+
+    // Verificação de segurança
+    if (index >= 0 && index < 3) {
+        printf("\nAs notas de %s são: %s e %s.\n", 
+                nomesAlunos[index][0], 
+                nomesAlunos[index][1], 
+                nomesAlunos[index][2]);
+    } else {
+        printf("\nErro: Aluno não encontrado! Escolha entre 0, 1 ou 2.\n");
+    }
 
     return 0;
 }
